ba12ecc0bdc5051ddad612f87d17505e7c36d2eb
Mehmet Uysal <muysal0103@gmail.com>
2025-12-18 01:46:42 +0300

    feat: add Dockerfile and GitHub Actions workflow

--- src/app.py
+++ src/app.py
@@ -174,4 +174,4 @@ def get_member_reservations(member_id):
     return jsonify({"reservations": my_classes})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
